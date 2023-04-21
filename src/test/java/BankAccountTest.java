a32f7defba1ddecbb83abea89270bed43c667935
ysbllcby <ysabellacubay@gmail.com>
2023-04-20 20:57:33 -0600

    Display name.

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("BankAccountTest")
 public class BankAccountTest {
 
     @Test
+    @DisplayName("Withdraw 300 from 500")
     public void testWithdraw() {
         BankAccount account = new BankAccount(500, -1000);
         account.withdraw(300);
@@ -11,6 +14,7 @@ public class BankAccountTest {
     }
 
     @Test
+    @DisplayName("Deposit 300 to 500")
     public void testDeposit() {
         BankAccount account = new BankAccount(500, -1000);
         account.deposit(300);
